13338c0aa7db5f7c7d0e5ab7404ae1b62d6117be
Krisetn Nestler <kristen.nestler@gmail.com>
2022-08-09 12:58:12 -0400

    add keys to hash

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -25,6 +25,10 @@ class Enigma
     # C << key[2,2].to_i
     # D << key[3,2].to_i
 
+  def keys
+    key_hash = Hask.new(A: @key[0..1].to_i, B: @key[1..2].to_i, C: @key[2..3].to_i, D: @key[3..4].to_i)
+  end
+
 
 
 end
