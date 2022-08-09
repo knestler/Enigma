13b6c66e9b7eba5edeb6da9b2047610a8caab572
Krisetn Nestler <kristen.nestler@gmail.com>
2022-08-09 12:57:24 -0400

    add random key generator

--- lib/enigma.rb
+++ lib/enigma.rb
@@ -10,4 +10,21 @@ class Enigma
 
   end
 
+  def encrypt(messege, key, date)
+    # encrypted = key[0]
+    # hash = {encryption: messege, key: key, date: date}
+    # return hash
+  end
+
+  def generated_key
+    5.times.map {rand(0..9)}
+  end 
+
+    # A << key[0,2].to_i
+    # B << key[1,2].to_i
+    # C << key[2,2].to_i
+    # D << key[3,2].to_i
+
+
+
 end
